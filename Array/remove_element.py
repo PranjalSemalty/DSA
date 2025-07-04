046a5531ab21fc54e088bbbbdc89669f3988a365
PranjalSemlaty <pranjalsemalty@gmail.com>
2025-07-04 22:05:37 +0530

    Day 4: solved two problems

---
+++ Array/remove_element.py
@@ -0,0 +1,14 @@
+# https://neetcode.io/problems/remove-element?list=neetcode250
+# NeetCode 250 - Arrays & Hashing
+# Difficulty: Easy
+
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k
+    
+
